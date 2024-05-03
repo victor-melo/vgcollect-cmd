1d2e2020d2fd7e8c427d4b5e19cb5dc7aa9e03e6
Victor <vmelo@victormelo.com>
2024-05-02 21:36:34 -0400

    updated output UI

--- vgcollectFunctions/_collection.py
+++ vgcollectFunctions/_collection.py
@@ -59,41 +59,69 @@ class collection(object):
           )
           return string
 
+     def checkBox(self, string):
+          if (string == "Yes"):
+               return "✓"
+          else:
+               return "✘"
+
      def fixStringOutputConsole(self,string):
-          string = (string.replace('Nintendo Entertainment System ', 'NES ') # Shortens the console NES
-          )
+          string = (string.replace('Nintendo Entertainment System ', 'NES ')) # Shortens the console NES
+          string = (string.replace('Super Nintendo ', 'SNES ')) # Shortens the console SNES
+          string = (string.replace('Game Boy Advance ', 'GBA ')) # Shortens the console GBA
+          string = (string.replace('Nintendo Switch ', 'Switch ')) # Shortens the console Switch
+          string = (string.replace('PlayStation Portable ', 'PSP ')) # Shortens the console PSP
+          string = (string.replace('PlayStation 2 ', 'PS2 ')) # Shortens the console PS2
+          string = (string.replace('PlayStation 2 ', 'PS2 ')) # Shortens the console PS2
+          string = (string.replace('PlayStation 3 ', 'PS3 ')) # Shortens the console PS3
+          string = (string.replace('PlayStation 4 ', 'PS4 ')) # Shortens the console PS4
+          string = (string.replace('PlayStation 5 ', 'PS5 ')) # Shortens the console PS5
+          string = (string.replace('PlayStation 6 ', 'PS6 ')) # Shortens the console PS6
           return string
 
      def search(self, query, use_local):
           
-          result = [["Console", "Title", "Cart", "Box", "Manual", "Notes", "Cost"], ["---","---", "---", "---", "---", "---", "---"]]
-
+          result1 = [["Platform", "Title", "Cart", "Box", "Manual"], ["----","----", "----", "----", "----"]]
+          result2 = []
           if (use_local == False):
                for line in self.collection.iter_lines():
                     if (self.fixStringInput(str(line)).find(self.fixStringInput(self.fixStringInput(query))) != -1):
-                         result.append(
+                         result1.append(
                               [
                                    self.fixStringOutputConsole(str(line).split("\",\"")[2]), # Console
                                    self.fixStringOutput(str(line).split("\",\"")[1]), # Title\
-                                   str(line).split("\",\"")[4], # Cart
-                                   str(line).split("\",\"")[5], # Box
-                                   str(line).split("\",\"")[6], # Manual
+                                   self.checkBox(str(line).split("\",\"")[4]), # Cart
+                                   self.checkBox(str(line).split("\",\"")[5]), # Box
+                                   self.checkBox(str(line).split("\",\"")[6]), # Manual
+                              ])
+
+                         result2.append(
+                              [
                                    str(line).split("\",\"")[3], # Notes
                                    str(line).split("\",\"")[8], # Price
+                                   "",
+                                   "",
+                                   ""
                               ])
           else:
                # If the person added the use_local flag, search the local cache
                for line in self.collection_local.splitlines():
                     if (self.fixStringInput(str(line)).find(self.fixStringInput(self.fixStringInput(query))) != -1):
-                         result.append(
+                         result1.append(
                               [
                                    self.fixStringOutputConsole(str(line).split("\",\"")[2]), # Console
                                    self.fixStringOutput(str(line).split("\",\"")[1]), # Title
-                                   str(line).split("\",\"")[4], # Cart
-                                   str(line).split("\",\"")[5], # Box
-                                   str(line).split("\",\"")[6], # Manual
+                                   self.checkBox(str(line).split("\",\"")[4]), # Cart
+                                   self.checkBox(str(line).split("\",\"")[5]), # Box
+                                   self.checkBox(str(line).split("\",\"")[6]), # Manual
+                              ])
+                         result2.append(
+                              [
+                                   "Cost: " + str(line).split("\",\"")[8], # Price
                                    str(line).split("\",\"")[3], # Notes
-                                   str(line).split("\",\"")[8], # Price
+                                   "",
+                                   "",
+                                   ""
                               ])
                          
 
@@ -104,21 +132,57 @@ class collection(object):
           init(autoreset=True)
           
           color_cycle = False
-          header_printed = False
+          header_printed1 = False
+          header_printed2 = False
 
           lens = []
-          for col in zip(*result):
+          lens2 = []
+
+          for col in zip(*result1):
                lens.append(max([len(v) for v in col]))
+          for col in zip(*result2):
+               lens2.append(max([len(v) for v in col]))
+          
+          index = 0
+
+          # The intention here is to figure out how wide each column is. Since we moved the notes & price column
+          # to the next line, we need to figure out whether the title or notes field is the longest column
+          print (lens)
+          print (lens2)
+          for l in lens:
+               # if (index < 2): # this is the header row.
+               #      index = index + 1
+               #      continue
+               if (l < lens2[index]):
+                    lens[index] = lens2[index]
+                    print ("test" + str(index))
+               index = index + 1
+          print (lens)
+          
+
           format = "  ".join(["{:<" + str(l) + "}" for l in lens])
-          for row in result:
-               if (not header_printed):
+          index = -2
+
+          for row in result1:
+               #print (row)
+               if (not header_printed1):
                     print(Fore.BLACK + Back.LIGHTCYAN_EX + format.format(*row))
-                    header_printed = True
+                    #print (result2[index])
+                    #print(Fore.BLACK + Back.LIGHTCYAN_EX + format.format(*result2[index]))
+                    header_printed1 = True
+               elif (not header_printed2):
+                    print(Fore.WHITE + Back.LIGHTBLACK_EX + format.format(*row))
+                    header_printed2 = True
                elif (color_cycle):
-                    print(Fore.WHITE + Back.LIGHTBLACK_EX+ format.format(*row))
+                    print(Fore.WHITE + Back.LIGHTBLACK_EX + format.format(*row))
+                    
+                    print(Fore.WHITE + Back.LIGHTBLACK_EX + format.format(*result2[index]))
                else:
                     print(Fore.BLACK + Back.CYAN  + format.format(*row))
+                    
+                    print(Fore.BLACK + Back.CYAN  + format.format(*result2[index]))
                color_cycle = not color_cycle
+               index = index+1
 
 
 
